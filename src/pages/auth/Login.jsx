7e24f63394ed265ae1332035f3602be5082c452e
thiagosilva <thiago.silva@imd.ufrn.br>
2020-10-13 14:43:49 -0300

    Material UI inserido nos inputs e botões

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,17 +1,90 @@
-import React from 'react'
+import React, { useState } from 'react'
+import clsx from 'clsx'
+import { withStyles, makeStyles } from '@material-ui/core/styles'
+import Input from '@material-ui/core/Input'
+import InputLabel from '@material-ui/core/InputLabel'
+import InputAdornment from '@material-ui/core/InputAdornment'
+import FormControl from '@material-ui/core/FormControl'
+import TextField from '@material-ui/core/TextField'
+import Visibility from '@material-ui/icons/Visibility'
+import VisibilityOff from '@material-ui/icons/VisibilityOff'
+import Button from '@material-ui/core/Button'
+import IconButton from '@material-ui/core/IconButton'
+import { blue, grey } from '@material-ui/core/colors'
 
 import logo from '../../img/logo.png'
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  margin: {
+    margin: theme.spacing(1),
+  },
+  textField: {
+    width: '100%',
+  },
+}))
+
+const DefaultButton = withStyles((theme) => ({
+  root: {
+    color: grey[600],
+    backgroundColor: 'transparent',
+    '&:hover': {
+      backgroundColor: 'transparent',
+      color: grey[700]
+    },
+    '&:focus': {
+      backgroundColor: 'transparent',
+      color: grey[800]
+    },
+  },
+}))(Button)
+
+const PrimaryButton = withStyles((theme) => ({
+  root: {
+    color: theme.palette.getContrastText(blue[500]),
+    backgroundColor: blue[500],
+    '&:hover': {
+      backgroundColor: blue[700],
+      color: grey[100]
+    },
+    '&:focus': {
+      backgroundColor: blue[800],
+      color: grey[100]
+    },
+  },
+}))(Button)
+
 const Login = () => {
+  const classes = useStyles()
+  const [values, setValues] = useState({
+    password: '',
+    showPassword: false,
+  })
+
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value })
+  }
+
+  const handleClickShowPassword = () => {
+    setValues({ ...values, showPassword: !values.showPassword })
+  }
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault()
+  }
+
   return (
     <>
       <div className='wrapper wrapper-full-page'>
         {/* you can change the color of the filter page using: filter-color='blue | purple | green | orange | red | rose' */}
-        <div className='page-header login-page header-filter bg-image' filter-color='black'>
+        <div className='page-header login-page header-filter bg-login-image' filter-color='black'>
           <div className='container'>
             <div className='row'>
               <div className='col-lg-6 col-md-6 col-sm-8 mx-auto'>
-                <form className='form' method='' action=''>
+                <form className={classes.root + 'form'}>
                   <div className='card card-login card-hidden'>
                     <div className='card-header card-header-blue text-center'>
                       <img src={logo} className='logotype' alt='Logo da Solumedi' />
@@ -21,28 +94,50 @@ const Login = () => {
                       <p className='card-description text-center'>Ir para o dashboard</p>
                       <span className='bmd-form-group'>
                         <div className='input-group'>
-                          <div className='input-group-prepend'>
-                            <span className='input-group-text'>
-                              <i className='material-icons'>email</i>
-                            </span>
-                          </div>
-                          <input type='email' className='form-control' placeholder='E-mail' />
+                          <FormControl className={clsx(classes.margin, classes.textField)}>
+                            <TextField
+                              required
+                              id="standard-email-input"
+                              label="E-mail"
+                              type="email"
+                              autoComplete="current-email"
+                            />
+                          </FormControl>
                         </div>
                       </span>
                       <span className='bmd-form-group'>
                         <div className='input-group'>
-                          <div className='input-group-prepend'>
-                            <span className='input-group-text'>
-                              <i className='material-icons'>lock_outline</i>
-                            </span>
-                          </div>
-                          <input type='password' className='form-control' placeholder='Digite sua senha' />
+                          <FormControl className={clsx(classes.margin, classes.textField)}>
+                            <InputLabel htmlFor="standard-adornment-password">Digite sua senha</InputLabel>
+                            <Input
+                              required
+                              id="standard-adornment-password"
+                              type={values.showPassword ? 'text' : 'password'}
+                              value={values.password}
+                              onChange={handleChange('password')}
+                              endAdornment={
+                                <InputAdornment position="end">
+                                  <IconButton
+                                    aria-label="alternar a visibilidade da senha"
+                                    onClick={handleClickShowPassword}
+                                    onMouseDown={handleMouseDownPassword}
+                                  >
+                                    {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                                  </IconButton>
+                                </InputAdornment>
+                              }
+                            />
+                          </FormControl>
                         </div>
                       </span>
                     </div>
-                    <div className='card-footer justify-content-center btn-auth-group'>
-                      <button className='btn btn-transparent btn-link btn-lg'>Esqueceu a senha?</button>
-                      <button className='btn btn-blue btn-link btn-lg'><strong>ENTRAR</strong></button>
+                    <div className='card-footer justify-content-center'>
+                      <DefaultButton href="#" size="small" color="default" className={classes.margin}>
+                        Esqueceu a senha?
+                      </DefaultButton>
+                      <PrimaryButton href="#" variant="contained" size="large" color="primary" className={classes.margin}>
+                        PRÓXIMO
+                      </PrimaryButton>
                     </div>
                   </div>
                 </form>
